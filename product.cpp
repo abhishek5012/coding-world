9749f9b3c42d8a953195f47a46228fe700523b44
abhishek5012 <90345464+abhishek5012@users.noreply.github.com>
2021-10-07 02:10:54 -0700

    Add files via upload

---
+++ product.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+	float x;
+	float y;
+	cout<<"enter the first value :";
+	cin>>x;
+	cout<<"enter the second value :";
+	cin>>y;
+	cout<<"the product of the two numbers is :"<<x*y;
+	return 0;
+	
+}
