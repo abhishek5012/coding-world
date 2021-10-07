f329f8258b154a302cff285a8eaad73c41bcc90f
abhishek5012 <90345464+abhishek5012@users.noreply.github.com>
2021-10-07 02:14:07 -0700

    Add files via upload

---
+++ size operator.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+int x;
+char y;
+float z;
+double w;
+	
+cout<<"size of the character is :"<<sizeof(y)<<" bytes"<<"\n";
+cout<<"size of the integer is :"<<sizeof(x)<<" bytes"<<"\n";
+cout<<"size of the float is :"<<sizeof(z)<<" bytes"<<"\n";
+cout<<"size of the double is :"<<sizeof(w)<<" bytes"<<"\n";	
+return 0;	
+}
